d347d9d43ddb80a13331841b59b1463f12fb6b43
Sasha Marchuk <misqbus@gmail.com>
2017-10-09 00:52:04 +0300

    Finish CSS without slider.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,19 @@
 var autoprefixer = require('gulp-autoprefixer'),
     gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    uncss = require('gulp-uncss');
+    rename = require('gulp-rename');
 
 
 // adding properties prefixes, minimazing and renaming css file
 gulp.task('css', function() {
-    gulp.src('styles/*.css')
+    gulp.src('css/*.css')
         .pipe(autoprefixer({ browsers: ['last 2 versions', '> 1%', 'ie 9'], cascade: false}))
         .pipe(minifyCSS())
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('styles/'))
-});
-
-
-// delete all useless css properties
-gulp.task('default', function () {
-    return gulp.src('styles/styles.css')
-        .pipe(uncss({
-            html: ['index.html']
-        }))
-        .pipe(gulp.dest('styles/uncss/'));
+        .pipe(gulp.dest('css/'))
 });
 
 // watch
 gulp.task('watch', function() {
-    gulp.watch('styles/*.css', ['css'])
+    gulp.watch('css/*.css', ['css'])
 });
