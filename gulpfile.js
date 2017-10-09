925e4796050491e95fc6f2219aafb816863ac107
Sasha Marchuk <misqbus@gmail.com>
2017-10-10 01:03:33 +0300

    Finish CSS slider.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var autoprefixer = require('gulp-autoprefixer'),
+    csscomb = require('gulp-csscomb'),
     gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     rename = require('gulp-rename');
 
 
-// adding properties prefixes, minimazing and renaming css file
+// adding properties prefixes, minimazing and renaming CSS file
 gulp.task('css', function() {
-    gulp.src('css/*.css')
-        .pipe(autoprefixer({ browsers: ['last 2 versions', '> 1%', 'ie 9'], cascade: false}))
+    gulp.src('css/styles.css')
+        .pipe(autoprefixer())
+        .pipe(csscomb())
+        .pipe(rename('styles.cross.css'))
+        .pipe(gulp.dest('css/'))
+});
+
+// Minify CSS file
+gulp.task('min', function() {
+    gulp.src('css/styles.cross.css')
         .pipe(minifyCSS())
         .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('css/'))
 });
 
 // watch
-gulp.task('watch', function() {
+gulp.task('default', function() {
     gulp.watch('css/*.css', ['css'])
 });
