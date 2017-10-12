fe3556d71b2d612ff106d7b995c5288308869313
Sasha Marchuk <misqbus@gmail.com>
2017-10-12 23:34:46 +0300

    Create shadows for slider buttons.

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,16 @@ var autoprefixer = require('gulp-autoprefixer'),
 
 // adding properties prefixes, minimazing and renaming CSS file
 gulp.task('css', function() {
-    gulp.src('css/styles.css')
-        .pipe(autoprefixer())
+    gulp.src('css/styles.work.css')
+        .pipe(autoprefixer({browsers: ['> 1%'], cascade: true}))
         .pipe(csscomb())
-        .pipe(rename('styles.cross.css'))
+        .pipe(rename('styles.css'))
         .pipe(gulp.dest('css/'))
 });
 
 // Minify CSS file
 gulp.task('min', function() {
-    gulp.src('css/styles.cross.css')
+    gulp.src('css/styles.css')
         .pipe(minifyCSS())
         .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('css/'))
@@ -24,5 +24,5 @@ gulp.task('min', function() {
 
 // watch
 gulp.task('default', function() {
-    gulp.watch('css/*.css', ['css'])
+    gulp.watch('css/*.css', ['css', 'min'])
 });
